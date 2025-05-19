35428ba9a3a02f6224fb8a80106f227e1a880b1c
Leo7479 <shubhrajitsarkar123ss@gmail.com>
2025-05-20 03:10:56 +0530

    Templates done with Header all done

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Login = ()=>{
+
+    return(
+        <div>
+            <div className="leftSide">
+
+            </div>
+
+            <div className="rightSide">
+                <span><Link to="/">Back to Home</Link></span>
+            </div>
+        </div>
+    );
+
+}
+
+export default Login;
